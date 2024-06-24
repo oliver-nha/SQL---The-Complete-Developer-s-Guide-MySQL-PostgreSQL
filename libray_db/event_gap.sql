9dd36962ce4bd32030db73fb18356a40beff0804
olivernha <nayhtetaung201733@example.com>
2024-06-24 22:21:42 +0800

    one folder deletion done

--- libray_db/event_gap.sql
+++
@@ -1,7 +0,0 @@
-
-select event1,event2,max(gapdays) as Longestgap
-(select e1.eventName as event1,e2.eventName as event2,
-julianday(e2.eventName) - julianday(e1.eventName) as gapdays
-from events e1
-JOIN events e2
-on e1.eventDate < e2.eventDate)
